3befcc551b23905b3cfadc0b7522ba6365de49ad
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-07-30 19:09:33 -0300

    conteudo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import About from './components/About'
+import Users from './components/Users'
 
 class App extends Component {
   render() {
     return (
-      <Home />
+    <BrowserRouter>
+      <div><Link to='/'>Home</Link></div>
+      <div><Link to='/about'>About</Link></div>
+      <div><Link to='/users'>Users</Link></div>
+      <div><Link to='/strict-access'>Strict Access</Link></div>
+      <Switch>     
+        <Route exact path='/' component={ Home } />
+        <Route exact path='/about' component={ About } />
+        <Route 
+          exact
+          path='/users/:id'
+          render={ (propsRoute) => <Users {...propsRoute} greetingMessage='Good Morning' /> }
+        />
+      </Switch>     
+    </BrowserRouter>  
     );
   }
 }
---
+++ src/components/StrictAccess.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class StrictAccess extends Component {
+  render() {
+    const { username, password } = this.props.user;
+    if(!(username === 'joao' && password === '1234')){
+        alert('Acess denied');
+        return <Redirect to='/' />
+    }
+    return (
+      <div>
+        Welcome { username }!
+      </div>
+    );
+  }
+};
+
+export default StrictAccess;
